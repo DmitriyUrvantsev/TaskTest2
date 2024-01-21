4002fb63ccdc1e16d138af0a3bdbb9ba6f1d764e
Dimon <urvandimon@gmail.com>
2024-01-21 19:15:03 +0300

    work

--- 1_hottel_1/lib/main.dart
+++ 1_hottel_1/lib/main.dart
@@ -1,91 +1,32 @@
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-
-// import 'package:hottel_1/x_core/x_utils/size_utils.dart';
-// import 'package:hottel_1/x_core/x_utils/x%20_pref_utils.dart%2020-17-13-099.dart';
-// import 'package:hottel_1/x_routes/app_routes.dart';
-// import 'package:hottel_1/x_theme/theme.dart';
-// import 'package:hottel_1/x_theme/x_provider/theme_provider.dart';
-// //import 'package:hottel_1/x_theme/x_provider/theme_provider.dart';
-// import 'package:provider/provider.dart';
-
-
-// //
-
-//  void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   Future.wait([
-//     SystemChrome.setPreferredOrientations([
-//       DeviceOrientation.portraitUp,
-//     ]),
-//     PrefUtils().init()//!======================= из новой
-//   ]).then((value) {
-//     runApp(MyApp());
-//   });
-// }
-
-// class MyApp extends StatelessWidget {
-
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Sizer(
-//       builder: (context, orientation, deviceType) {
-//         return ChangeNotifierProvider(
-//           create: (context) => ThemeProvider(),
-//           child: Consumer(
-//             builder: (BuildContext context, provider, Widget? child) {
-//               return HotelAppWidget();
-//             },
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class HotelAppWidget extends StatelessWidget {
-//   const HotelAppWidget({
-//     super.key,
-
-//   });
-
-//     static final mainNavigation = MainNavigation();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         title: 'Hottel App',
-//         debugShowCheckedModeBanner: false,
-//         theme: basicTheme(),
-//         routes: mainNavigation.routes,
-//         initialRoute: mainNavigation.initialRoute,
-//         onGenerateRoute: mainNavigation.onGenerateRoutes);
-//   }
-// }
-
-//!=============================================================================
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:hottel_1/x_core/x_utils/size_utils.dart';
+import 'package:hottel_1/x_core/x_utils/x%20_pref_utils.dart%2020-17-13-099.dart';
 import 'package:hottel_1/x_routes/app_routes.dart';
 import 'package:hottel_1/x_theme/theme.dart';
+import 'package:hottel_1/x_theme/x_provider/theme_provider.dart';
+//import 'package:hottel_1/x_theme/x_provider/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized(); //!для асинхронности
- 
-  runApp(const MyApp());
-  
+//
+
+ void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      //DeviceOrientation.portraitUp,
+    ]),
+    PrefUtils().init()//!======================= из новой
+  ]).then((value) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
-
-  static final mainNavigation = MainNavigation();
 
   const MyApp({Key? key}) : super(key: key);
 
@@ -93,15 +34,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-        print(orientation);
-        return MaterialApp(
-            title: 'Hottel App',
-            debugShowCheckedModeBanner: false,
-            theme: basicTheme(),
-                        routes: mainNavigation.routes,
-            initialRoute: mainNavigation.initialRoute,
-            onGenerateRoute: mainNavigation.onGenerateRoutes);
+        return ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+          child: Consumer(
+            builder: (BuildContext context, provider, Widget? child) {
+              return HotelAppWidget();
+            },
+          ),
+        );
       },
     );
   }
 }
+
+class HotelAppWidget extends StatelessWidget {
+  const HotelAppWidget({
+    super.key,
+
+  });
+
+    static final mainNavigation = MainNavigation();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Hottel App',
+        debugShowCheckedModeBanner: false,
+        theme: basicTheme(),
+        routes: mainNavigation.routes,
+        initialRoute: mainNavigation.initialRoute,
+        onGenerateRoute: mainNavigation.onGenerateRoutes);
+  }
+}
+
+//!=============================================================================
+
+
